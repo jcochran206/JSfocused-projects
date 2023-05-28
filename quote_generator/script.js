6d2321ac68604fd54f33bbd063c3c9a77ff247d3
jcochran206 <jonathan.o.cochran@gmail.com>
2023-05-27 20:01:07 -0700

    added function to call api and get elements by id

--- quote_generator/script.js
+++ quote_generator/script.js
@@ -1,1 +1,30 @@
-alert('test');
+//Dom elements to target html 
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+const twitterBtn = document.getElementById('twitter');
+
+//get quotes and set into array
+let apiQuotes = []; 
+//new quote request from user
+function newQuote(){
+    let randomQuote = Math.floor(Math.random() * apiQuotes.length); 
+    return console.log(apiQuotes[randomQuote]);
+}
+
+
+async function getQuotes() {
+    //randomize quotes
+    //call function to display
+    const apiURL = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch(error){
+        console.log(error, 'error')
+    }
+}
+//callback
+getQuotes();
