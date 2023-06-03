76d0c883d60d082b3cffad2578dc41723b70bee7
jcochran206 <jonathan.o.cochran@gmail.com>
2023-06-02 23:27:32 -0700

    created function to get photos

--- infinite_scroll/script.js
+++ infinite_scroll/script.js
@@ -1,1 +1,20 @@
-alert('hi');
+//limit for photos
+const count = 10;
+const apiKey = 'key here';
+//unsplash API 
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//Get photos from unsplaced fetch request 
+async function getPhotos(){
+    try {
+        const response = await fetch(apiURL);
+        const jsonData = await response.json();
+        console.log(jsonData);
+
+    }catch (error){
+        console.log(error, 'did not fetch unsplash data')
+    }
+}
+
+//on load 
+getPhotos();
