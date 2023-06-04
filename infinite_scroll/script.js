d186433769d6396fe5b9054a10faee1ebfaad4b0
jcochran206 <jonathan.o.cochran@gmail.com>
2023-06-03 23:28:55 -0700

    added dom elements, display function, and created ui elements

--- infinite_scroll/script.js
+++ infinite_scroll/script.js
@@ -1,6 +1,33 @@
+//UI elements to interact with 
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+//array of photos
+let photosArray = [];
+
+//Create elements for links and photos, add to dom
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        // create <a> to link to unsplash 
+        const item = document.createElement("a");
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        //create an img for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        //put <img> inside <a>, then both inside image container
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+
+    });
+}
+
+
 //limit for photos
 const count = 10;
-const apiKey = 'key here';
+const apiKey = 'api key here';
 //unsplash API 
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -8,8 +35,9 @@ const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getPhotos(){
     try {
         const response = await fetch(apiURL);
-        const jsonData = await response.json();
-        console.log(jsonData);
+        photosArray = await response.json();
+        displayPhotos(); 
+        console.log(photosArray);
 
     }catch (error){
         console.log(error, 'did not fetch unsplash data')
