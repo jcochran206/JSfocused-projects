be213aa983f4945f10bb29b05fbb3e2b8b37801d
jcochran206 <jonathan.o.cochran@gmail.com>
2023-06-28 22:36:47 -0700

    added tellme function and hoisted joke from getJokes function then added event listener to play rss and hide audio element

--- joke-teller/app.js
+++ joke-teller/app.js
@@ -4,10 +4,13 @@ const audioElement = document.getElementById('audio');
 //text to speech api from https://www.voicerss.org/ 
 const VoiceRSS={speech(e){this._validate(e),this._request(e)},_validate(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){let a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw`The browser does not support the audio codec ${e.c}`}},_request(e){const a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;let e=t.responseText;audioElement.src=e,audioElement.onloadedmetadata=(()=>{audioElement.play()})}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest(e){const a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return`key=${e.key||""}&src=${e.src||""}&hl=${e.hl||""}&r=${e.r||""}&c=${a||""}&f=${e.f||""}&ssml=${e.ssml||""}&b64=true`},_detectCodec(){const e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
 
-function test(){
+
+// Passing joke to VoiceRss API
+function tellMe(joke){
+    console.log('tell me', joke);
     VoiceRSS.speech({
-        key: 'API Key',
-        src: 'Hello, world!',
+        key: 'API Key here',
+        src: `${joke}`,
         hl: 'en-us',
         v: 'Linda',
         r: 0, 
@@ -15,6 +18,24 @@ function test(){
         f: '44khz_16bit_stereo',
         ssml: false
     });
-    
 }
-test();
+
+//Get Jokes utilizing fetch function with try catch 
+async function getJokes(){
+    let joke = '';
+    const url = 'https://v2.jokeapi.dev/joke/Any';
+    try {
+    const response = await fetch(url);
+    let data = await response.json();
+        if(data.setup){
+            joke = `${data.setup} ... ${data.delivery};`
+        }else{
+            joke = data.joke;
+        }
+        tellMe(joke)
+    } catch(error){//catch errror here
+        console.log(`fetch error: ${error.message}`)
+    }
+}
+
+button.addEventListener('click', getJokes);
