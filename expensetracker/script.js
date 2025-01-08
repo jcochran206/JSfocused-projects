143c550b72a08158569b42d533e163ed165e6aea
jcochran206 <jonathan.o.cochran@gmail.com>
2025-01-07 23:17:16 -0800

    added functions for selecting months, saving data, and loading expenses

--- expensetracker/script.js
+++ expensetracker/script.js
@@ -33,10 +33,29 @@ document.addEventListener('DOMContentLoaded', () => {
         December: { Housing: 0, Food: 0, Transportation: 0, Bills: 0, Miscellaneous: 0 }
     };
     //load expenses
-
+    function getExpensesFromLocalStorage(month,year){
+        const key = `${month}-${year}`;
+        return JSON.parse(localStorage.getItem(key)) || {};
+    }
     //save expenses 
-
+    function saveExpensesToLocalStorage(month,year){
+        const key = `${month}-${year}`;
+        localStorage.setItem(key, JSON.stringify(expenses[month]));
+    }
     //get selected month and year
+    function getSelectMonthYear(){
+        selectedMonth = monthSelect.value;
+        selectedYear = yearSelect.value;
+
+        if(!selectedMonth || !selectedYear){
+            alert('Month or year not selected');
+            return;
+        }
+
+        if(!expenses[selectedMonth]){
+            expenses[selectedMonth] = {Housing : 0, Food: 0, Transportation: 0, Bills: 0, Miscellaneous: 0 }
+        }
+    }
 
     //visualize data
 
