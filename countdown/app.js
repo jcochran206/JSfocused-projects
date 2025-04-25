527f840d60bb4b33d877e5702f172aebfdb82203
jcochran206 <jonathan.o.cochran@gmail.com>
2025-04-25 00:48:42 -0700

    countdown function

---
+++ countdown/app.js
@@ -0,0 +1,35 @@
+/* elements need to display days hours mins */
+const dayselm = document.getElementById('days');
+const hourselm = document.getElementById('countdown');
+const minuteselm = document.getElementById('minutes');
+
+/* set date we are count down to */
+const countDownDate = new Date("June 9, 2025 10:00:00").getTime();
+
+/* create function to conduct countdown and conditional for function reaching zero */
+function countDownTarget(targetDateString, countdown){
+    const targetDate = new Date(targetDateString);
+
+    function updateCountdown(){
+        const now = new Date();
+        const timeleft = targetDate - now;
+
+        if(timeleft <= 0){
+            document.getElementById(countdown).innerText = "countdown finished";
+            clearInterval(intervalId);
+            return;
+        }
+
+        const days = Math.floor(timeleft/ (1000 * 60 * 60 * 24));
+        const hours = Math.floor(timeleft/ (1000 * 60 * 60) % 24);
+        const minutes = Math.floor(timeleft/ (1000 * 60) % 60);
+
+        dayselm.innerText = `${days} D`;
+        hourselm.innerText = `${hours} H`;
+        minuteselm.innerText = `${minutes} M`;
+    }
+
+    const intervalId = setInterval(updateCountdown, 1000);
+    updateCountdown();
+    
+}
